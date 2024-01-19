d10e7e160495cdd933d4b5ab2ad0f463c51e4a9d
monarxxjain <jainmonark1000@gmail.com>
2024-01-20 00:17:40 +0530

    Updated

--- values.js
+++ values.js
@@ -1,8 +1,8 @@
 const values = {};
 
 // values.url = "http://localhost:5001";
-values.url = "https://marco-dashboard-backend.vercel.app";
-// values.url = "https://swiftstay-backend.vercel.app";
+// values.url = "https://marco-dashboard-backend.vercel.app";
+values.url = "https://swiftstay-backend.vercel.app";
 
 values.profileMenu = [
   {
